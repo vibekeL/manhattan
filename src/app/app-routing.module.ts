c2ecfacdf715df62094db2081b0115ad03004dc2
Vibeke Larsen <vanl@itu.dk>
2021-06-07 15:24:54 +0200

    remove test comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SlotsComponent } from './slots/slots.component';
 import { SlotDetailComponent } from './slot-detail/slot-detail.component';
-import { TestTableComponent } from './test-table/test-table.component';
-
 
 const routes: Routes = [
   { path: '', redirectTo: '/slots', pathMatch: 'full' },
   { path: 'slots', component: SlotsComponent },
-  { path: 'temp', component: TestTableComponent },
   { path: 'detail/:id', component: SlotDetailComponent },
 ];
 
